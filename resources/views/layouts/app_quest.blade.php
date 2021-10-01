27d8d06bb6a6f25e8639edb8085df298c92b5096
Pitter <PITTER775@GAMIL.COM>
2021-10-01 18:35:11 -0300

    .

--- resources/views/layouts/app_quest.blade.php
+++ resources/views/layouts/app_quest.blade.php
@@ -9,26 +9,26 @@
     <meta name="theme-color" content="#612400">
     <meta content="pitter775@gmail.com" name="author">
     <meta property="og:description" content="{{$titulopesq}}"/>
-    <meta property="og:image" content="{{ asset('img') }}/logowhats.jpg" />
+    <meta property="og:image" content="https://xb3-laravel.vercel.app/img/logowhats.jpg" />
     <meta property="og:type" content="website"/>
     <meta property="og:locale" content="pt_BR"/> 
 
     <link rel="apple-touch-icon" sizes="76x76" href="">
-    <link rel="icon" type="image/png" href="{{ asset('img') }}/logop.png">
+    <link rel="icon" type="image/png" href="https://xb3-laravel.vercel.app/img/logop.png">
 
     <title> XB3 Soluções - Pesquisa </title>
 
     <!-- CSS Files -->
-    <link href="{{ asset('vendor') }}/bootstrap/css/bootstrap.min.css" rel="stylesheet" />
-    <link href="{{ asset('vendor') }}/icofont/icofont.min.css" rel="stylesheet" />
-    <link href="{{ asset('vendor') }}/boxicons/css/boxicons.min.css" rel="stylesheet" />
-    <link href="{{ asset('vendor') }}/venobox/venobox.css" rel="stylesheet" />
-    <link href="{{ asset('vendor') }}/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet" />
-    <link href="{{ asset('vendor') }}/aos/aos.css" rel="stylesheet" />
-    <link href="{{ asset('vendor') }}/md/mdb.min.css" rel="stylesheet" />
+    <link href="https://xb3-laravel.vercel.app/bootstrap/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="https://xb3-laravel.vercel.app/icofont/icofont.min.css" rel="stylesheet" />
+    <link href="https://xb3-laravel.vercel.app/boxicons/css/boxicons.min.css" rel="stylesheet" />
+    <link href="https://xb3-laravel.vercel.app/venobox/venobox.css" rel="stylesheet" />
+    <link href="https://xb3-laravel.vercel.app/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet" />
+    <link href="https://xb3-laravel.vercel.app/aos/aos.css" rel="stylesheet" />
+    <link href="https://xb3-laravel.vercel.app/md/mdb.min.css" rel="stylesheet" />
     <link href="https://fonts.googleapis.com/css2?family=Titillium+Web:ital,wght@0,200;0,300;0,400;0,600;0,700;0,900;1,200;1,300;1,400;1,600;1,700&display=swap" rel="stylesheet">
     <!-- Template Main CSS File -->
-    <link href="{{ asset('css') }}/style.css" rel="stylesheet" />
+    <link href="https://xb3-laravel.vercel.app/css/style.css" rel="stylesheet" />
 
     <!-- Global site tag (gtag.js) - Google Analytics -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=G-R3RDVZG5HK"></script>
@@ -49,21 +49,21 @@
 
 
     <!-- Vendor JS Files -->
-    <script src="{{ asset('vendor') }}/jquery/jquery.min.js"></script>
-    <script src="{{ asset('vendor') }}/jquery/jquery-ui.min.js"></script>
-    <script src="{{ asset('vendor') }}/bootstrap/js/bootstrap.bundle.min.js"></script>
-    <script src="{{ asset('vendor') }}/jquery.easing/jquery.easing.min.js"></script>
-    <script src="{{ asset('vendor') }}/waypoints/jquery.waypoints.min.js"></script>
-    <script src="{{ asset('vendor') }}/counterup/counterup.min.js"></script>
-    <script src="{{ asset('vendor') }}/isotope-layout/isotope.pkgd.min.js"></script>
-    <script src="{{ asset('vendor') }}/venobox/venobox.min.js"></script>
-    <script src="{{ asset('vendor') }}/owl.carousel/owl.carousel.min.js"></script>
-    <script src="{{ asset('vendor') }}/aos/aos.js"></script>
-    <script src="{{ asset('vendor') }}/md/mdb.min.js"></script>
+    <script src="https://xb3-laravel.vercel.app/jquery/jquery.min.js"></script>
+    <script src="https://xb3-laravel.vercel.app/jquery/jquery-ui.min.js"></script>
+    <script src="https://xb3-laravel.vercel.app/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="https://xb3-laravel.vercel.app/jquery.easing/jquery.easing.min.js"></script>
+    <script src="https://xb3-laravel.vercel.app/waypoints/jquery.waypoints.min.js"></script>
+    <script src="https://xb3-laravel.vercel.app/counterup/counterup.min.js"></script>
+    <script src="https://xb3-laravel.vercel.app/isotope-layout/isotope.pkgd.min.js"></script>
+    <script src="https://xb3-laravel.vercel.app/venobox/venobox.min.js"></script>
+    <script src="https://xb3-laravel.vercel.app/owl.carousel/owl.carousel.min.js"></script>
+    <script src="https://xb3-laravel.vercel.app/aos/aos.js"></script>
+    <script src="https://xb3-laravel.vercel.app/md/mdb.min.js"></script>
     <script src="http://jqueryvalidation.org/files/dist/jquery.validate.js"></script>
     <!-- Template Main JS File -->
-    <script src="{{ asset('js') }}/main.js"></script>
-    <!-- <script src="{{ asset('js') }}/whats.js"></script> -->
+    <script src="https://xb3-laravel.vercel.app/js/main.js"></script>
+    <!-- <script src="https://xb3-laravel.vercel.app/js/whats.js"></script> -->
     <!-- <script defer async src='https://duz4dqsaqembt.cloudfront.net/client/whats.js'></script> -->
 
     @stack('scripts')
