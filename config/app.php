4c85e4b495346d84f13fc9611369485fc7617e21
Pitter <PITTER775@GAMIL.COM>
2021-10-01 18:19:08 -0300

    .

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://xb3-laravel-pitter775.vercel.app'),
 
     'asset_url' => env('ASSET_URL', null),
 
