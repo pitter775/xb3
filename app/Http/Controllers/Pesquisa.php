28603dc1de747133811c16ca12376ef306666e46
Pitter <PITTER775@GAMIL.COM>
2021-10-04 09:51:11 -0300

    .

--- app/Http/Controllers/Pesquisa.php
+++ app/Http/Controllers/Pesquisa.php
@@ -9,6 +9,8 @@ class Pesquisa extends Controller
     public function index($id){
         $json = file_get_contents("https://dev-survy.xb3solucoes.com.br/rest/survey/".$id."");  
         $data = json_decode($json);
+
+        dd($data);
         if($data->alreadyAnswered){
             return view("pages.questoes_ok", compact('data'));
         }
