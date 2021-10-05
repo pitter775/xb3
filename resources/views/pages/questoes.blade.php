965fa0ae033a94f1c68b664450cdd4b4b3fbddd2
Pitter <PITTER775@GAMIL.COM>
2021-10-05 09:03:58 -0300

    .

--- resources/views/pages/questoes.blade.php
+++ resources/views/pages/questoes.blade.php
@@ -113,7 +113,7 @@ console.log('. json_encode( $data )
     console.log('lista');
     console.log(lista);
 
-    if(!$lista['alreadyAnswered']){
+    if(lista['alreadyAnswered']){
         $('.centertela').show();
         $(".obrigado").delay(1000).animate({   opacity: '1', }, 1200, null);
         $(".obrigado2").delay(1800).animate({   opacity: '1', }, 500, null);      
