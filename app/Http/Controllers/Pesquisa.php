0f0a5f1e50ad69171d98766a96b988ae3e4f5b89
Pitter <PITTER775@GAMIL.COM>
2021-10-05 09:15:17 -0300

    .

--- app/Http/Controllers/Pesquisa.php
+++ app/Http/Controllers/Pesquisa.php
@@ -9,7 +9,6 @@ class Pesquisa extends Controller
     public function index($id){
         $json = file_get_contents("https://dev-survy.xb3solucoes.com.br/rest/survey/".$id."");  
         $data = json_decode($json);
-        $this->console_log( $data );
 
         $dadosid = $id;
 
@@ -19,10 +18,4 @@ class Pesquisa extends Controller
         }
         return view("pages.questoes", compact('data','dadosid'));
     }
-
-    function console_log( $data ){
-        echo '<script>';
-        echo 'console.log('. json_encode( $data ) .')';
-        echo '</script>';
-      }
 }
